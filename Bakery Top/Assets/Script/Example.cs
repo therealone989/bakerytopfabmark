6851a364ef3603a2fd04b638d486e7a6096d799c
MarkiBoy69 <markriemann.mr@gmail.com>
2025-01-24 20:25:50 +0100

    Example

--- Bakery Top/Assets/Script/Example.cs
+++ Bakery Top/Assets/Script/Example.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 
 public class Example : MonoBehaviour
 {
+    [SerializeField] float speed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Move();
+    }
+
+    public void Move()
     {
         
     }
