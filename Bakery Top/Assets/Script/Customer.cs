7745217c3ae45c27575e4690fc973040c0fff149
MarkiBoy69 <markriemann.mr@gmail.com>
2025-01-27 20:02:31 +0100

    Customer Throw

--- Bakery Top/Assets/Script/Customer.cs
+++ Bakery Top/Assets/Script/Customer.cs
@@ -1,9 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public class Customer : MonoBehaviour
 {
     [SerializeField] private ItemSeller itemSeller;
+    [SerializeField] public GameObject moneyPrefab;
+    [SerializeField] public Transform moneyspawnPoint;
+    [SerializeField] public float throwForce = 5f;
     private InventoryManager iM;
+
 
     private void Start()
     {
@@ -12,8 +17,14 @@ public class Customer : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        HandleItem(collision);
+    }
+
+
+    public void HandleItem(Collision collision)
+    {
         Item item = collision.collider.GetComponent<Item>();
-        if(item != null)
+        if (item != null)
         {
             // Ruft das ItemSO vom Item-Skript ab
             ItemSO itemData = item.GetItemData();
@@ -25,13 +36,33 @@ public class Customer : MonoBehaviour
 
                 // Zerstˆrt das Objekt nach dem Verkauf
                 Destroy(collision.collider.gameObject);
+
+                StartCoroutine(ThrowMoneyWithDelay(2f));
             }
             else
             {
-                Debug.LogWarning("Das Item hat keine g¸ltigen Daten und kann nicht verkauft werden.");
+                Debug.LogWarning("Das Item hat keine gÅEtigen Daten und kann nicht verkauft werden.");
             }
-
         }
     }
+    private IEnumerator ThrowMoneyWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
 
+        if(moneyPrefab != null && moneyspawnPoint != null)
+        {
+            GameObject money = Instantiate(moneyPrefab, moneyspawnPoint.position, Quaternion.identity);
+
+            Rigidbody rb = money.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                Vector3 throwDirection = (moneyspawnPoint.forward + Vector3.up).normalized;
+                rb.AddForce(throwDirection * throwForce, ForceMode.Impulse);
+            }
+            else
+            {
+                Debug.LogWarning("MoneyPrefab oder MoneySpawnPoint ist nicht zugewiesen.");
+            }
+        }
+    }
 }
