22191e6b4a9eb672645c8be7c60a9150aea13f56
MarkiBoy69 <markriemann.mr@gmail.com>
2025-01-31 15:28:16 +0100

    Wolken added + CarSpawner und Carscript geändert

--- Bakery Top/Assets/Script/CarSpawner.cs
+++ Bakery Top/Assets/Script/CarSpawner.cs
@@ -6,6 +6,7 @@ public class CarSpawner : MonoBehaviour
     public Transform spawnPoint;  // Der Punkt, an dem die Autos erscheinen sollen
     public float spawnInterval = 8f;  // Intervall in Sekunden, wie oft ein Auto spawnt
     public bool isLeft; // Gibt an, ob das Auto nach links fahren soll
+    public bool randomScale = false;
 
     private void Start()
     {
@@ -16,6 +17,12 @@ public class CarSpawner : MonoBehaviour
     {
         Quaternion rotation = isLeft ? Quaternion.Euler(0, 0, 0) : Quaternion.Euler(0, 180, 0);
         Instantiate(carPrefab, spawnPoint.position, rotation);
+
+        if (randomScale)
+        {
+            float randomScaleFactor = Random.Range(0.5f, 2f);  // Skalierung zwischen 0.5 und 2
+            carPrefab.transform.localScale = new Vector3(randomScaleFactor, randomScaleFactor, randomScaleFactor);
+        }
     }
 
     private void B()
---
+++ Bakery Top/Assets/Script/ClowdMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ClowdMovement : MonoBehaviour
+{
+    public float speed = 5f;  // Geschwindigkeit des Autos
+
+    private void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);  // Auto bewegt sich in seine eigene Vorwärtsrichtung
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ende"))
+        {
+            Debug.Log("End reached");
+            Destroy(gameObject);
+        }
+    }
+}
