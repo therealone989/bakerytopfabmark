a5af512dd9f5a7590ec78024c3f244314e510318
MarkiBoy69 <markriemann.mr@gmail.com>
2025-01-31 02:41:23 +0100

    AUTOS SPAWNEN VERDAMMTE SAU!!!111!!

---
+++ Bakery Top/Assets/Script/CarMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public float speed = 5f;  // Geschwindigkeit des Autos
+    public float pushForce = 10f;  // Wie stark das Auto den Spieler wegschiebt
+    public float upwardPush = 5f;  // Wie stark das Auto den Spieler nach oben schiebt
+
+    private void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);  // Auto bewegt sich in seine eigene Vorwärtsrichtung
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Start Collision");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody playerRb = collision.gameObject.GetComponent<Rigidbody>();
+            if (playerRb != null)
+            {
+                // Richtung berechnen
+                Vector3 pushDirection = collision.transform.position - transform.position;
+                pushDirection.y = 0;  // Nur horizontal schieben
+
+                // Spieler nach hinten und oben schleudern
+                playerRb.AddForce(pushDirection.normalized * pushForce, ForceMode.Impulse);
+                playerRb.AddForce(Vector3.up * upwardPush, ForceMode.Impulse);
+            }
+        }
+        if (collision.gameObject.CompareTag("Ende"))
+        {
+            Debug.Log("End reached");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Bakery Top/Assets/Script/CarSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject carPrefab;  // Das Prefab des Autos
+    public Transform spawnPoint;  // Der Punkt, an dem die Autos erscheinen sollen
+    public float spawnInterval = 8f;  // Intervall in Sekunden, wie oft ein Auto spawnt
+    public bool isLeft; // Gibt an, ob das Auto nach links fahren soll
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(B), 0f, spawnInterval);  // Autos spawnen alle X Sekunden
+    }
+
+    private void A(bool isLeft)
+    {
+        Quaternion rotation = isLeft ? Quaternion.Euler(0, 0, 0) : Quaternion.Euler(0, 180, 0);
+        Instantiate(carPrefab, spawnPoint.position, rotation);
+    }
+
+    private void B()
+    {
+        A(isLeft); // Ruft A() mit dem gesetzten isLeft-Wert auf
+    }
+}
